6eec799ecb33eba749ebb855625dc60452670115
Miguel Pragier <miguelpragier@gmail.com>
2018-06-13 13:55:39 -0300

    Update comments for documentation

--- handy.go
+++ handy.go
@@ -3,7 +3,6 @@ package handy
 import (
 	"crypto/sha256"
 	"fmt"
-	"log"
 	"math/rand"
 	"regexp"
 	"strconv"
@@ -14,6 +13,7 @@ import (
 )
 
 // CheckPersonName returns true if the name contains at least two words, one >= 3 chars and one >=2 chars.
+// I understand that this is a particular criteria, but this is the OpenSourceMagic, where you can change and adapt to your own specs.
 func CheckPersonName(name string, acceptEmpty bool) bool {
 	name = strings.TrimSpace(name)
 
@@ -52,7 +52,47 @@ func CheckPersonName(name string, acceptEmpty bool) bool {
 	return found2 && found3
 }
 
+// CheckCompanyName returns true if the name contains at least two words, digits allowed, one >= 3 chars and one >=2 chars.
+// The main difference from CheckpersonName is that CheckCompanyName accept numbers/digits.
+// I understand that this is a particular criteria, but this is the OpenSourceMagic, where you can change and adapt to your own specs.
+func CheckCompanyName(name string, acceptEmpty bool) bool {
+	name = strings.TrimSpace(name)
+
+	if name == "" {
+		return acceptEmpty
+	}
+
+	if name != OnlyLettersAndNumbers(name) {
+		return false
+	}
+
+	a := strings.Fields(name)
+
+	if len(a) < 2 {
+		return false
+	}
+
+	// At least two words, one with 3 chars and other with 2
+	found2 := false
+	found3 := false
+
+	for _, s := range a {
+		if !found3 && utf8.RuneCountInString(s) >= 3 {
+			found3 = true
+			continue
+		}
+
+		if !found2 && utf8.RuneCountInString(s) >= 2 {
+			found2 = true
+			continue
+		}
+	}
+
+	return found2 && found3
+}
+
 // CheckCPF returns true if the given sequence is a valid cpf
+// CPF is the Brazilian TAXPayerID document for persons
 func CheckCPF(cpf string) bool {
 	// Se já chegar vazio, falha
 	if cpf == "" {
@@ -103,6 +143,7 @@ func CheckCPF(cpf string) bool {
 
 // CheckCNPJ returns true if the cnpj is valid
 // Thanks to https://gopher.net.br/validacao-de-cpf-e-cnpj-em-go/
+// CNPJ is the Brazilian TAXPayerID document for companies
 func CheckCNPJ(cnpj string) bool {
 	cnpj = OnlyDigits(cnpj)
 
@@ -211,17 +252,20 @@ func CheckDate(yyyymmdd string) bool {
 
 	if _, err := time.Parse(time.RFC3339, s); err == nil {
 		return true
-	} else {
-		log.Println(err)
 	}
 
 	return false
 }
 
-// PorExtenso recebe um int64 e retorna o valor por extenso.
-// Ex: PorExtenso(129) => "cento e vinte e nove"
-// Suporta até um trilhão e não acrescenta vírgulas.
-func PorExtenso(n int64) string {
+// AmountAsWord receives an int64 e returns the value as its text representation
+// Today I have only the PT-BR text.
+// Ex: AmountAsWord(129) => "cento e vinte e nove"
+// Supports up to one trillion and does not add commas.
+func AmountAsWord(n int64) string {
+	if n == 0 {
+		return "zero"
+	}
+
 	negativo := n < 0
 	menos := ""
 
@@ -249,14 +293,14 @@ func PorExtenso(n int64) string {
 		quantos := n / 10
 		resto := n % 10
 
-		a := [...]string{"", "", "vinte", "trinta", "quarenta", "cinqüenta", "sessenta", "setenta", "oitenta", "noventa"}
+		a := [...]string{"", "", "vinte", "trinta", "quarenta", "cinquenta", "sessenta", "setenta", "oitenta", "noventa"}
 		e := " "
 
 		if resto > 0 {
 			e = " e "
 		}
 
-		return strings.Replace(fmt.Sprintf("%s%s%s%s", menos, a[quantos], e, PorExtenso(resto)), "  ", " ", -1)
+		return strings.Replace(fmt.Sprintf("%s%s%s%s", menos, a[quantos], e, AmountAsWord(resto)), "  ", " ", -1)
 	}
 
 	if n < mil {
@@ -274,7 +318,7 @@ func PorExtenso(n int64) string {
 			e = " e "
 		}
 
-		return strings.Replace(fmt.Sprintf("%s%s%s%s", menos, a[quantos], e, PorExtenso(resto)), "  ", " ", -1)
+		return strings.Replace(fmt.Sprintf("%s%s%s%s", menos, a[quantos], e, AmountAsWord(resto)), "  ", " ", -1)
 	}
 
 	if n < umMilhao {
@@ -290,7 +334,7 @@ func PorExtenso(n int64) string {
 			e = " e "
 		}
 
-		return strings.Replace(fmt.Sprintf("%s%s mil %s%s", menos, PorExtenso(quantos), e, PorExtenso(resto)), "  ", " ", -1)
+		return strings.Replace(fmt.Sprintf("%s%s mil %s%s", menos, AmountAsWord(quantos), e, AmountAsWord(resto)), "  ", " ", -1)
 	}
 
 	if n < umBilhao {
@@ -311,7 +355,7 @@ func PorExtenso(n int64) string {
 			e = " e "
 		}
 
-		return strings.Replace(fmt.Sprintf("%s%s%s%s%s", menos, PorExtenso(quantos), u, e, PorExtenso(resto)), "  ", " ", -1)
+		return strings.Replace(fmt.Sprintf("%s%s%s%s%s", menos, AmountAsWord(quantos), u, e, AmountAsWord(resto)), "  ", " ", -1)
 	}
 
 	if n < umTrilhao {
@@ -332,7 +376,7 @@ func PorExtenso(n int64) string {
 			e = " e "
 		}
 
-		return strings.Replace(fmt.Sprintf("%s%s%s%s%s", menos, PorExtenso(quantos), u, e, PorExtenso(resto)), "  ", " ", -1)
+		return strings.Replace(fmt.Sprintf("%s%s%s%s%s", menos, AmountAsWord(quantos), u, e, AmountAsWord(resto)), "  ", " ", -1)
 	}
 
 	if n == umTrilhao {
@@ -342,6 +386,8 @@ func PorExtenso(n int64) string {
 	return fmt.Sprintf("%s%d", menos, n)
 }
 
+// Run some basic checks on new password strings
+// My rule requires at least six chars, with at least one letter and at least one number.
 func CheckNewPassword(password, passwordConfirmation string) (bool, string) {
 	const minPasswordLength = 6
 
@@ -368,7 +414,8 @@ func CheckNewPassword(password, passwordConfirmation string) (bool, string) {
 	return true, ""
 }
 
-func PasswordHash(password string) string {
+// StringHash simply generates a SHA256 hash from the given string
+func StringHash(password string) string {
 	h := sha256.New()
 
 	h.Write([]byte(password))
@@ -378,6 +425,7 @@ func PasswordHash(password string) string {
 	return fmt.Sprintf("%x", sum)
 }
 
+// OnlyAlpha returns only the letters from the given string, after strip all the rest ( numbers, spaces, etc. )
 func OnlyAlpha(sequence string) string {
 	if utf8.RuneCountInString(sequence) == 0 {
 		return ""
@@ -394,6 +442,7 @@ func OnlyAlpha(sequence string) string {
 	return string(letters)
 }
 
+// OnlyDigits returns only the numbers from the given string, after strip all the rest ( letters, spaces, etc. )
 func OnlyDigits(sequence string) string {
 	if utf8.RuneCountInString(sequence) > 0 {
 		re, _ := regexp.Compile("[\\D]")
@@ -404,6 +453,7 @@ func OnlyDigits(sequence string) string {
 	return sequence
 }
 
+// OnlyLettersAndNumbers returns only the letters and numbers from the given string, after strip all the rest, like spaces and special symbols.
 func OnlyLettersAndNumbers(sequence string) string {
 	if utf8.RuneCountInString(sequence) == 0 {
 		return ""
@@ -420,18 +470,21 @@ func OnlyLettersAndNumbers(sequence string) string {
 	return string(aplhanumeric)
 }
 
+// RandomInt returns a rondom integer within the given (inclusive) range
 func RandomInt(min, max int) int {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	return rand.Intn(max) + min
 }
 
+// CheckPhone returns true if a given sequence has between 9 and 14 digits
 func CheckPhone(phone string, acceptEmpty bool) bool {
 	phone = OnlyDigits(phone)
 
 	return (acceptEmpty && (phone == "")) || ((len([]rune(phone)) >= 9) && (len([]rune(phone)) <= 14))
 }
 
+// YMDasDateUTC returns a valid UTC time from the given yyymmdd-formatted sequence
 func YMDasDateUTC(yyyymmdd string, utc bool) (time.Time, error) {
 	yyyymmdd = OnlyDigits(yyyymmdd)
 
@@ -446,10 +499,13 @@ func YMDasDateUTC(yyyymmdd string, utc bool) (time.Time, error) {
 	}
 }
 
+// YMDasDate returns a valid time from the given yyymmdd-formatted sequence
 func YMDasDate(yyyymmdd string) (time.Time, error) {
 	return YMDasDateUTC(yyyymmdd, false)
 }
 
+// ElapsedMonths returns the number of elapsed months between two given dates
+// I have to re-check the both versions and choose one to stay. If you have a good suggestion, just tell me!
 func ElapsedMonths(fromDate, toDate time.Time) int {
 	// Se toDate for anterior a fromDate, retornar 0
 	if !fromDate.Before(toDate) {
@@ -493,6 +549,8 @@ func ElapsedMonths(fromDate, toDate time.Time) int {
 	return months
 }
 
+// ElapsedMonths2 returns the number of elapsed months between two given dates
+// I have to re-check the both versions and choose one to stay. If you have a good suggestion, just tell me!
 func ElapsedMonths2(fromDate, toDate time.Time) int {
 	// Se toDate for anterior a fromDate, retornar 0
 	if !fromDate.Before(toDate) {
@@ -538,32 +596,8 @@ func ElapsedMonths2(fromDate, toDate time.Time) int {
 	return months
 }
 
-/*func ElapsedYears(birthdate, today time.Time) int {
-	bday := birthdate.YearDay()
-
-	years := today.Year() - birthdate.Year()
-
-	leapy := func(d time.Time) bool {
-		year := d.Year()
-
-		return (year%400 == 0) || (year%100 != 0) || (year%4 == 0)
-	}
-
-	if (bday >= 60) && leapy(birthdate) && !leapy(today) {
-		return bday - 1
-	}
-
-	if (today.YearDay() >= 60) && !leapy(birthdate) && leapy(today) {
-		return bday + 1
-	}
-
-	if today.YearDay() < bday {
-		return years - 1
-	}
-
-	return years
-}
-*/
+// StringAsFloat tries to convert a string to float, and if it can't, just returns zero
+// It's limited to one billion
 func StringAsFloat(s string, decimalSeparator, thousandsSeparator rune) float64 {
 	if s == "" {
 		return 0.0
@@ -586,11 +620,25 @@ func StringAsFloat(s string, decimalSeparator, thousandsSeparator rune) float64 
 	return 0.0
 }
 
+// StringAsInteger returns the integer value extracted from string, or zero
+func StringAsInteger(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	if i, err := strconv.ParseInt(s, 10, 32); err == nil {
+		return int(i)
+	}
+
+	return 0
+}
+
 // Between checks if param n is greather or equal to param low and lower than or equal param high
 func Between(n, low, high int) bool {
 	return n >= low && n <= high
 }
 
+// Tif is a simple implementation of the dear ternary IF operator
 func Tif(condition bool, tifThen, tifElse interface{}) interface{} {
 	if condition {
 		return tifThen
@@ -599,6 +647,7 @@ func Tif(condition bool, tifThen, tifElse interface{}) interface{} {
 	}
 }
 
+// ElapsedYears returns the number of elapsed years between two given dates
 func ElapsedYears(from, to time.Time) int {
 	if from.IsZero() || to.IsZero() {
 		return 0
@@ -613,12 +662,14 @@ func ElapsedYears(from, to time.Time) int {
 	return elapsedYears
 }
 
+// YearsAge returns the number of years past since a given date
 func YearsAge(birthdate time.Time) int {
 	return ElapsedYears(birthdate, time.Now())
 }
 
-func Truncate( s string, maxLen int, trim bool) string {
-	if s=="" {
+// Truncate limits the length of a given string, trimming or not, according parameters
+func Truncate(s string, maxLen int, trim bool) string {
+	if s == "" {
 		return s
 	}
 
@@ -634,6 +685,7 @@ func Truncate( s string, maxLen int, trim bool) string {
 }
 
 const (
+	TransformNone                     = uint8(0)
 	TransformFlagTrim                 = uint8(2)
 	TransformFlagLowerCase            = uint8(4)
 	TransformFlagUpperCase            = uint8(8)
@@ -642,8 +694,17 @@ const (
 	TransformFlagOnlyLettersAndDigits = uint8(64)
 )
 
-func Transform( s string, maxLen int, transformFlags uint8) string {
-	if s=="" {
+// Transform handles a string according given flags/parametrization, as follows:
+// Available Flags to be used alone or combined:
+//	TransformNone - Does nothing. It's only for truncation.
+//	TransformFlagTrim - Trim spaces before and after proccess the input
+//	TransformFlagLowerCase - Change string case to lower
+//	TransformFlagUpperCase - Change string case to upper
+//	TransformFlagOnlyDigits - Filter/strip all but digits
+//	TransformFlagOnlyLetters - Filter/strip all but letters
+//	TransformFlagOnlyLettersAndDigits - Filter/strip all but numbers and letters. Removes spaces, punctuation and special symbols
+func Transform(s string, maxLen int, transformFlags uint8) string {
+	if s == "" {
 		return s
 	}
 
@@ -677,21 +738,22 @@ func Transform( s string, maxLen int, transformFlags uint8) string {
 	}
 
 	// Have to trim before and after, to avoid issues with string truncation and new leading/trailing spaces
-	if ( transformFlags & TransformFlagTrim ) == TransformFlagTrim {
+	if (transformFlags & TransformFlagTrim) == TransformFlagTrim {
 		s = strings.TrimSpace(s)
 	}
 
-	if ( transformFlags & TransformFlagLowerCase) == TransformFlagLowerCase {
+	if (transformFlags & TransformFlagLowerCase) == TransformFlagLowerCase {
 		s = strings.ToLower(s)
 	}
 
-	if ( transformFlags & TransformFlagUpperCase) == TransformFlagUpperCase {
+	if (transformFlags & TransformFlagUpperCase) == TransformFlagUpperCase {
 		s = strings.ToUpper(s)
 	}
 
 	return s
 }
 
+// MatchesAny returns true if any of the given items matches ( equals ) the subject ( search parameter )
 func MatchesAny(search interface{}, items ...interface{}) bool {
 	for _, v := range items {
 		if fmt.Sprintf("%T", search) != fmt.Sprintf("%T", v) {
@@ -704,6 +766,7 @@ func MatchesAny(search interface{}, items ...interface{}) bool {
 	return false
 }
 
+// HasOnlyNumbers returns true if the sequence is entirely numeric
 func HasOnlyNumbers(sequence string) bool {
 	if utf8.RuneCountInString(sequence) == 0 {
 		return false
@@ -718,6 +781,7 @@ func HasOnlyNumbers(sequence string) bool {
 	return true
 }
 
+// HasOnlyNumbers returns true if the sequence is entirely composed by letters
 func HasOnlyLetters(sequence string) bool {
 	if utf8.RuneCountInString(sequence) == 0 {
 		return false
@@ -730,4 +794,27 @@ func HasOnlyLetters(sequence string) bool {
 	}
 
 	return true
+}
+
+// TrimLen returns the runes count after trim the spaces
+func TrimLen(text string) int {
+	if text == "" {
+		return 0
+	}
+
+	text = strings.TrimSpace(text)
+
+	if text == "" {
+		return 0
+	}
+
+	return utf8.RuneCountInString(text)
+}
+
+// CheckMinLen verifies if the rune-count is greater then or equal the given minimum
+// It returns true if the given string has length greater than or equal than minLength parameter
+func CheckMinLen(value string, minLength int) bool {
+	value = strings.TrimSpace(value)
+
+	return TrimLen(value) >= minLength
 }
