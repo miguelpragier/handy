7a254beb918151b03829d0df52c75494e122c2b3
openvista.miguel <miguelpragier@openvistaglobal.com>
2019-11-25 15:20:18 -0300

    Working on RandomIntArray

--- handy.go
+++ handy.go
@@ -259,9 +259,18 @@ func RemoveDigits(sequence string) string {
 
 // RandomInt returns a random integer within the given (inclusive) range
 func RandomInt(min, max int) int {
-	//rand.Seed(time.Now().UTC().UnixNano())
-
 	return rand.Intn(max-min) + min
+}
+
+// RandomIntArray returns an array filled with random integer numbers
+func RandomIntArray(min, max, howMany int) []int {
+	var a []int
+
+	for i := 0; i < howMany; i++ {
+		a = append(a, RandomInt(min, max))
+	}
+
+	return a
 }
 
 // RandomReseed restarts the randonSeeder and returns a random integer within the given (inclusive) range
